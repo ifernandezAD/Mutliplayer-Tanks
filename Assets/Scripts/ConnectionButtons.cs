31a42b8494bc72d40a6b6b8642d0f14dc5cb780d
IFernandezAD <ifernandezad@gmail.com>
2023-09-23 15:40:37 +0200

    Movement changed to be client authorative

---
+++ Assets/Scripts/ConnectionButtons.cs
@@ -0,0 +1,16 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class ConnectionButtons : MonoBehaviour
+{
+    public void StartClient()
+    {
+        NetworkManager.Singleton.StartClient();
+    }
+
+    public void StartHost()
+    {
+        NetworkManager.Singleton.StartHost();
+    }
+
+}
