2f12e84e8a28efb25eaac0f4d1e8d0ed31e617a4
IFernandezAD <ifernandezad@gmail.com>
2023-09-28 16:25:32 +0200

    Starting making coin Spawning method

--- Assets/Scripts/Core/Coins/RespawningCoin.cs
+++ Assets/Scripts/Core/Coins/RespawningCoin.cs
@@ -1,5 +1,9 @@
+using System;
+
 public class RespawningCoin : Coin
 {
+    public event Action<RespawningCoin> OnCollected;
+
     public override int Collect()
     {
         if (!IsServer)
@@ -11,6 +15,13 @@ public class RespawningCoin : Coin
         if (alreadyCollected) { return 0; }
 
         alreadyCollected = true;
+        OnCollected?.Invoke(this);
+
         return coinValue;
+    }
+
+    public void Reset()
+    {
+        alreadyCollected = false;
     }
 }
