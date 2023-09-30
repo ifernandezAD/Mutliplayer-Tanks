99201ea95e8520a3f242ba1e16b3dffb31e40ebd
IFernandezAD <ifernandezad@gmail.com>
2023-09-30 17:23:39 +0200

    ApplicationController Structure set

---
+++ Assets/Scripts/Networking/Client/ClientSingleton.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ClientSingleton : MonoBehaviour
+{
+    private static ClientSingleton instance;
+
+    private ClientGameManager gameManager;
+
+    public static ClientSingleton Instance
+    {
+        get
+        {
+            if (instance != null) { return instance; }
+
+            instance = FindAnyObjectByType<ClientSingleton>();
+
+            if (instance == null)
+            {
+                Debug.Log("No ClientSingleton in the scene");
+                return null;
+            }
+
+            return instance;
+        }
+    }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public async Task CreateClient()
+    {
+        gameManager = new ClientGameManager();
+
+        await gameManager.InitAsync();
+    }
+}
